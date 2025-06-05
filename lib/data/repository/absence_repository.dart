66ea3df8b73bedde3d4acc93fb623c17429f6a68
Bhavneet Singh <bhavneet0812@gmail.com>
2025-06-05 05:57:23 +0530

    Add AbsenceRepository class for fetching absence and member details

---
+++ lib/data/repository/absence_repository.dart
@@ -0,0 +1,23 @@
+import 'package:frontend_coding_challenge/data/models/absence.dart';
+import 'package:frontend_coding_challenge/data/models/member.dart';
+import 'package:frontend_coding_challenge/data/services/api_service.dart';
+
+class AbsenceRepository {
+  const AbsenceRepository();
+
+  ApiService get apiService => ApiService.instance;
+
+  /// Fetches absence list and member details.
+  /// Returns a list of tuples containing [Absence] and corresponding [Member].
+  /// If a member is not found for an absence, the member will be null.
+  Future<List<(Absence, Member?)>> getAbsenceDetails() async {
+    final absences = await apiService.getAbsences();
+    final members = await apiService.getMembers();
+
+    return absences.map((absence) {
+      final memberIndex = members.indexWhere((m) => m.userId == absence.userId);
+      final member = memberIndex != -1 ? members[memberIndex] : null;
+      return (absence, member);
+    }).toList();
+  }
+}
