162baa2c5963521c947552761c11709ea646b14e
Bhavneet Singh <bhavneet0812@gmail.com>
2025-06-05 12:56:44 +0530

    Add AbsenceListFilterModel for filtering absence data by status, type, and date range

---
+++ lib/data/data_models/absence_list_filter_model.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:frontend_coding_challenge/core/utils/nullable_value.dart';
+import 'package:frontend_coding_challenge/data/enums/absence_status.dart';
+import 'package:frontend_coding_challenge/data/enums/absence_type.dart';
+
+class AbsenceListFilterModel {
+  final AbsenceStatus? status;
+  final AbsenceType? type;
+  final DateTimeRange? dateRange;
+
+  const AbsenceListFilterModel({this.status, this.type, this.dateRange});
+
+  AbsenceListFilterModel copyWith({
+    NullableValue<AbsenceStatus>? status,
+    NullableValue<AbsenceType>? type,
+    NullableValue<DateTimeRange>? dateRange,
+  }) {
+    return AbsenceListFilterModel(
+      status: status == null ? this.status : status.value,
+      type: type == null ? this.type : type.value,
+      dateRange: dateRange == null ? this.dateRange : dateRange.value,
+    );
+  }
+}
