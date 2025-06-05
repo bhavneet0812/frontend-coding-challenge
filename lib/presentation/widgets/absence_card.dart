a21c86b86f000d90a223530878013a04cc63c0bd
Bhavneet Singh <bhavneet0812@gmail.com>
2025-06-05 07:34:17 +0530

    Add AbsenceCard widget to display absence details for members

---
+++ lib/presentation/widgets/absence_card.dart
@@ -0,0 +1,50 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:frontend_coding_challenge/data/models/absence.dart';
+import 'package:frontend_coding_challenge/data/models/member.dart';
+
+class AbsenceCard extends StatelessWidget {
+  final Absence absence;
+  final Member member;
+
+  const AbsenceCard({super.key, required this.absence, required this.member});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: ListTile(
+        leading:
+            member.image?.isNotEmpty == true
+                ? CachedNetworkImage(
+                  imageUrl: member.image!,
+                  imageBuilder: (_, image) {
+                    return CircleAvatar(backgroundImage: image);
+                  },
+                  progressIndicatorBuilder: (_, _, dp) {
+                    return CircularProgressIndicator(value: dp.progress);
+                  },
+                  errorWidget: (_, _, _) {
+                    return CircleAvatar(child: Icon(Icons.error));
+                  },
+                )
+                : CircleAvatar(child: Icon(Icons.person)),
+        title: Text(member.name),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Type: ${absence.type.title}'),
+            Text(
+              'Period: ${absence.startDateFormatted} - ${absence.endDateFormatted}',
+            ),
+            if (absence.memberNote.isNotEmpty)
+              Text('Note: ${absence.memberNote}'),
+            if (absence.admitterNote.isNotEmpty)
+              Text('Admitter: ${absence.admitterNote}'),
+            Text('Status: ${absence.status.title}'),
+          ],
+        ),
+      ),
+    );
+  }
+}
