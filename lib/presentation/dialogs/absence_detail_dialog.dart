ca5e1069b320cf859030eb0acc1d49ce58143781
Bhavneet Singh <bhavneet0812@gmail.com>
2025-06-06 03:39:52 +0530

    Add AbsenceDetailDialog and integrate it into AbsenceCard for detailed absence view

--- lib/presentation/dialogs/absence_detail_dialog.dart
+++ lib/presentation/dialogs/absence_detail_dialog.dart
@@ -3,32 +3,26 @@ import 'package:flutter/material.dart';
 import 'package:frontend_coding_challenge/core/extensions/string_extension.dart';
 import 'package:frontend_coding_challenge/data/models/absence.dart';
 import 'package:frontend_coding_challenge/data/models/member.dart';
-import 'package:frontend_coding_challenge/presentation/dialogs/absence_detail_dialog.dart';
 
-class AbsenceCard extends StatelessWidget {
+class AbsenceDetailDialog extends StatelessWidget {
   final Absence absence;
   final Member member;
 
-  const AbsenceCard({super.key, required this.absence, required this.member});
+  const AbsenceDetailDialog({
+    super.key,
+    required this.absence,
+    required this.member,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      child: InkWell(
-        borderRadius: BorderRadius.circular(8),
-        onTap: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AbsenceDetailDialog(absence: absence, member: member);
-            },
-          );
-        },
+    return Dialog(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 4, left: 8, right: 8, bottom: 12),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             ListTile(
               leading:
@@ -78,7 +72,7 @@ class AbsenceCard extends StatelessWidget {
             ),
 
             Padding(
-              padding: const EdgeInsets.fromLTRB(20.0, 0, 20.0, 12.0),
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -89,15 +83,11 @@ class AbsenceCard extends StatelessWidget {
                         ? 'Member Note: ${absence.memberNote}'
                         : 'No additional notes from member.',
                     style: TextStyle(fontSize: 14, color: Colors.grey),
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
                   ),
                   if (absence.admitterNote.isNotEmpty)
                     Text(
                       'Admitter Note: ${absence.admitterNote}',
                       style: TextStyle(fontSize: 14, color: Colors.grey),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
                     ),
                 ],
               ),
