ba931d641afeb400a124cbe6e9ad60ebb7d8d7de
Bhavneet Singh <bhavneet0812@gmail.com>
2025-06-05 12:57:00 +0530

    Refactor getAbsences method to support filtering by status, type, and date range with pagination

--- lib/data/services/api_service.dart
+++ lib/data/services/api_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:frontend_coding_challenge/data/data_models/absence_list_filter_model.dart';
 import 'package:frontend_coding_challenge/data/models/absence.dart';
 import 'package:frontend_coding_challenge/data/models/member.dart';
 
@@ -13,15 +14,45 @@ class ApiService {
   /// Uses [compute] to parse the JSON data in a separate isolate for better performance.
   /// Returns a list of [Absence] objects.
   /// Throws an exception if the JSON file cannot be loaded or parsed.
-  Future<List<Absence>> getAbsences() async {
+  Future<(List<Absence> absences, int totalCount)> getAbsences({
+    AbsenceListFilterModel? filter,
+    int skip = 0,
+    int limit = 10,
+  }) async {
     final data = await rootBundle.loadString('assets/json_files/absences.json');
     final jsonResult = await compute(
       (message) => json.decode(data),
       'parseAbsencesJson',
     );
-    return (jsonResult['payload'] as List)
-        .map((e) => Absence.fromJson(e))
-        .toList();
+    final jsonList = jsonResult['payload'] as List;
+    final absences = jsonList.map((e) => Absence.fromJson(e)).toList();
+    // Apply filter if provided
+    if (filter != null) {
+      // Filter by absence status
+      if (filter.status != null) {
+        absences.removeWhere((absence) => absence.status != filter.status);
+      }
+
+      // Filter by absence type
+      if (filter.type != null) {
+        absences.removeWhere((absence) => absence.type != filter.type);
+      }
+
+      // Filter by date range
+      if (filter.dateRange != null) {
+        absences.removeWhere((absence) {
+          final startDate = absence.startDate;
+          final endDate = absence.endDate;
+          final filterStartDate = filter.dateRange!.start;
+          final filterEndDate = filter.dateRange!.end;
+
+          return !(startDate.isAfter(filterEndDate) ||
+              endDate.isBefore(filterStartDate));
+        });
+      }
+    }
+
+    return (absences.skip(skip).take(limit).toList(), absences.length);
   }
 
   /// Get members from the local JSON file.
